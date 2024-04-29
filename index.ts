41410dbdbdbf049ea0dc6e4afc6538988355db95
Sheena Mohsin <sheenamohsin123@gmail.com>
2024-04-29 18:18:42 +0500

    Word Counter

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {
+      name: "sentence",
+      type: "input",
+      message: "Enter a sentence"
+}
+]);
+const word = answer.sentence.trim().split(" ")
+console.log(word)
+console.log(`Total words in this sentence are: ${word.length}`)
